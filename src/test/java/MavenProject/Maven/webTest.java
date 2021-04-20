78ab6223917ad9aee2268a9a4eac6b7b8f8a0523
GS-1749 <poojaannaldas@gmail.com>
2021-04-20 10:45:05 +0530

    First Commit

---
+++ src/test/java/MavenProject/Maven/webTest.java
@@ -0,0 +1,20 @@
+package MavenProject.Maven;
+
+import org.testng.annotations.Test;
+
+public class webTest {
+	
+	@Test
+	public void login()
+	{
+		System.out.println("Login success");
+	}
+	
+	@Test
+	public void logout()
+	{
+		System.out.println("Logout success");
+	}
+	
+
+}
